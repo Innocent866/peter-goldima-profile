9bccb852d2602fb656270e04eca696a8444c1d7e
Innocent866 <igoldima@gmail.com>
2024-12-22 15:36:43 -0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,20 @@ import HeroPage from "./pages/HeroPage";
 import About from "./pages/AboutPage";
 import Contact from "./pages/Contact";
 import Footer from "./component/Footer";
-import NotFound from "./pages/NotFound";
+// import NotFound from "./pages/NotFound";
 
 
 
 const App = () => {
   return (
-    <div className="app">
+    <div>
           <BrowserRouter>
           <Navbar/>
             <Routes>
               <Route path="/" element={<HeroPage/>} />
               <Route path="/about" element={<About/>} />
               <Route path="/contact" element={<Contact/>} />
-              <Route path="*" element={<NotFound/>} />
+              {/* <Route path="*" element={<NotFound/>} /> */}
             </Routes>
             <Footer/>
           </BrowserRouter>
